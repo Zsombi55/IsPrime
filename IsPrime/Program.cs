00df19b329732692f95f966d7a04416b1b0805a5
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-11-23 11:33:39 +0200

    IsPrime ok. Next..IsDeletable.

--- IsPrime/Program.cs
+++ IsPrime/Program.cs
@@ -5,8 +5,9 @@
  * Time: 09:56
  * 
  * Primes are numbers that can only be divided cleanly by 1 or themselves.
- *  If removing any single digit still results in a prime, and if shrinking it down to one digit
- * it still remains a prime, then the "original" was a deletable prime.
+ *  If removing any single digit still results in a prime, and if shrinking it down to one digit it still remains a prime,
+ * then the "original" was a Deletable Prime; if also true, the number of viable "routes" are to be noted, eg.:
+ * 4125673:12, 41256793:21, 537430451:3, 200899998:0, 537499093:8, 2147483059:8
  * 
  * THIS IS INCOMPLETE. I wanted to upload this code anyway, as proof that I have tried to code it.
  */
@@ -19,17 +20,17 @@ namespace IsPrime
 		public static void Main(string[] args)
 		{
 			Program P = new Program();
-			int n = 537430451;
+			double n = 537430451; // Prime?: true | Deletable?: true, 3.
 			
-			Console.WriteLine("Is prime?" + P.isPrime(int n));
+			Console.WriteLine("Is prime? {0}", P.IsPrime(n));
 			
 //			if (isPrime(n)) {Console.WriteLine("Is deletable? " + isDeletable(n));}
 
 			Console.ReadLine();
 		}
 		
-		private bool isPrime(int n) {
-			int sqrtN=Math.floor(Math.sqrt(n));   // no decimals
+		private bool IsPrime(double n) {
+			double sqrtN = Math.Floor(Math.Sqrt(n));   // no decimals
 			bool p = n != 1;     // boolean
 			for(int i = 2; i < sqrtN + 1; i++) {
 				if(n % i == 0) {
@@ -39,5 +40,7 @@ namespace IsPrime
 			}
 			return p;
 		}
+		
+		private bool IsDeletable(double n){}
 	}
 }
