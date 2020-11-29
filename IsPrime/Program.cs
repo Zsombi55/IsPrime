13c523fe86fefb8b0bfce1553a8ac840a19237d9
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-11-29 15:59:50 +0200

    User console input & validity check.

--- IsPrime/Program.cs
+++ IsPrime/Program.cs
@@ -20,21 +20,32 @@ namespace IsPrime
 		public static void Main(string[] args)
 		{
 			Program P = new Program();
-			double n = 537430451; // Prime?: true | Deletable?: true, viable routes: 3.
+			double number = 537430451; // for quick testing.  Prime?: true | Deletable?: true, viable routes: 3.
 			
-			Console.WriteLine("Is prime? {0}", P.IsPrime(n));
+/* - while testing.
 			
-			if (P.IsPrime(n)) {
-				Console.WriteLine("Is deletable? ", P.IsDeletable(n));
-			}
+  			Console.WriteLine("Enter number:");
+			string data = Console.ReadLine();
+			
+			if(Double.TryParse(data, out number)) // Validity check.
+				Console.WriteLine("Is  {0}  a prime? {1}.", number, P.IsPrime(number));				
+			else
+				Console.WriteLine("'{0}' is not a valid number.", data);
+*/			
+			Console.WriteLine("Is  {0}  a prime? {1}.", number, P.IsPrime(number));
+			
+//			if (P.IsPrime(n)) {
+//				Console.WriteLine("Is deletable? ", P.IsDeletable(n));
+//			}
 
-			Console.ReadLine();
+			Console.ReadKey();
+//			EndApp();
 		}
-		
+
 // ----- Is Prime -----
 		private bool IsPrime(double n) {
 			double sqrtN = Math.Floor(Math.Sqrt(n));   // no decimals
-			bool p = n != 1;     // boolean
+			bool p = (n != 1);     // boolean
 			for(int i = 2; i < sqrtN + 1; i++) {
 				if(n % i == 0) {
 					p = false;
@@ -45,40 +56,42 @@ namespace IsPrime
 		}
 		
 // ----- Is Deletable ----- // clearly there are C# method/syntax differences from JS.
-		private bool IsDeletable(double n) {
-			string s = n.ToString();
-			bool d = true;
-			
-			TruncateLeft(s);
-			TruncateRight(s);
-			
-			if (!d) {
-				d = false;
-				break;
-			}
-			return d;
-		}
-		
-		private void TruncateLeft(string s) { // truncate sequence from left
-			int le = s.Length;
 
-			for (int i = 0 ; i < le; i++) {
-				string tl = s.Slice(i, le);
-				if (IsPrime(string tl)) {
-					Console.WriteLine(tl);
-				}
-			}
-		}
-
-		private void TruncateRight(string s) { // truncate sequence from right
-			int le = s.Length;
-
-			for (int i = 0 ; i < le; i++) {
-				string tr = s.Slice(0, le - i);
-				if (IsPrime(string tr)) {
-					Console.WriteLine(tr);
-				}
-			}
-		}
+// some misc temp code
+//		private bool IsDeletable(double n) {
+//			string s = n.ToString();
+//			bool d = true;
+//			
+//			TruncateLeft(s);
+//			TruncateRight(s);
+//			
+//			if (!d) {
+//				d = false;
+//				break;
+//			}
+//			return d;
+//		}
+//		
+//		private void TruncateLeft(string s) { // truncate sequence from left
+//			int le = s.Length;
+//
+//			for (int i = 0 ; i < le; i++) {
+//				string tl = s.Slice(i, le);
+//				if (IsPrime(string tl)) {
+//					Console.WriteLine(tl);
+//				}
+//			}
+//		}
+//
+//		private void TruncateRight(string s) { // truncate sequence from right
+//			int le = s.Length;
+//
+//			for (int i = 0 ; i < le; i++) {
+//				string tr = s.Slice(0, le - i);
+//				if (IsPrime(string tr)) {
+//					Console.WriteLine(tr);
+//				}
+//			}
+//		}
 	}
 }
