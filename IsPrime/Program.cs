0161531b41c0db0c6ba1b0a836a89dbce13fcf19
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-11-30 20:06:33 +0200

    Solved, found several solutions fixes.
    Remove useless digits, get "real" length, solution for alternatingly cut out digits and returning what remains. Deletable check ok on 1st level. Need all viable routrs/elements checked. Like building a tree graph.

--- IsPrime/Program.cs
+++ IsPrime/Program.cs
@@ -12,6 +12,7 @@
  * THIS IS INCOMPLETE. Have not figured out how to code the Deletable check.
  */
 using System;
+using System.Text;
 
 namespace IsPrime
 {
@@ -20,28 +21,57 @@ namespace IsPrime
 		public static void Main(string[] args)
 		{
 			Program P = new Program();
-			double number = 537430451; // for quick testing.  Prime?: true | Deletable?: true, viable routes: 3.
+			double number = -0053743045.100; // Test, length: 9.  Prime?: true | Deletable?: true, viable routes: 3.
 			
 /* - while testing.
 			
-  			Console.WriteLine("Enter number:");
+  			Console.WriteLine("Enter number (valid symbols are the negative & decimal markers):");
 			string data = Console.ReadLine();
 			
-			if(Double.TryParse(data, out number)) // Validity check.
-				Console.WriteLine("Is  {0}  a prime? {1}.", number, P.IsPrime(number));				
+			if(Double.TryParse(data, out number)) // Validity check; also removes unnecesary zeroes.
+				Console.WriteLine("Is  {0}  a prime? {1}.\r\n", number, P.IsPrime(number));				
 			else
 				Console.WriteLine("'{0}' is not a valid number.", data);
-*/			
-			Console.WriteLine("Is  {0}  a prime? {1}.", number, P.IsPrime(number));
+*/
+			Console.WriteLine("Is  {0}  a prime? {1}.\r\n", number, P.IsPrime(number)); // - Test.
 			
-//			if (P.IsPrime(n)) {
-//				Console.WriteLine("Is deletable? ", P.IsDeletable(n));
-//			}
+			if (P.IsPrime(number)) {
+				P.HowLong(number);
+				Console.WriteLine("Is deletable? {0}", P.IsDeletable(number));
+			}
 
 			Console.ReadKey();
 //			EndApp();
 		}
 
+		private void HowLong(double n) { // check 'data' instead after test !
+			string sn = n.ToString();
+			int length = sn.Length;
+			if(sn.Contains("-")) length--;
+			if(sn.Contains(",")) length--;
+			if(sn.Contains(".")) length--;
+			Console.WriteLine("Length: {0}", length);
+		}
+		
+//TODO: either in HowLong or below : remove decimal & minus markers before truncating to make it easier
+//			there is no Math done that would make a differnce in the outcome,
+//			unless one wishes to display all viable route elements, then they would need to be put back..
+//			or just skip them during truncating upon encountering them under the "target index" of the respective loop cycle ??
+
+//TODO: so far it performs only "1st level" check.. the viable elements of it are not checked.
+//			Each ok element has to be stored then later ran through the same check.. and so on
+//			until there are no more viable routes OR all single digit ones have been checked.
+//		It's basically like building a tree graph with the start node being the user given value and all child/leaf nodes the viable elements.
+	
+		private void TruncateLeft(string s) { // truncate sequence from left
+			int le = s.Length;
+			for (int i = 0; i < le; ++i) {
+				StringBuilder sb = new StringBuilder(s);
+				sb.Remove(i, 1);
+				Console.WriteLine(sb);
+			}
+		}
+		
 // ----- Is Prime -----
 		private bool IsPrime(double n) {
 			double sqrtN = Math.Floor(Math.Sqrt(n));   // no decimals
@@ -55,43 +85,18 @@ namespace IsPrime
 			return p;
 		}
 		
-// ----- Is Deletable ----- // clearly there are C# method/syntax differences from JS.
+// ----- Is Deletable -----
 
-// some misc temp code
-//		private bool IsDeletable(double n) {
-//			string s = n.ToString();
-//			bool d = true;
-//			
-//			TruncateLeft(s);
-//			TruncateRight(s);
-//			
-//			if (!d) {
-//				d = false;
-//				break;
-//			}
-//			return d;
-//		}
-//		
-//		private void TruncateLeft(string s) { // truncate sequence from left
-//			int le = s.Length;
-//
-//			for (int i = 0 ; i < le; i++) {
-//				string tl = s.Slice(i, le);
-//				if (IsPrime(string tl)) {
-//					Console.WriteLine(tl);
-//				}
-//			}
-//		}
-//
-//		private void TruncateRight(string s) { // truncate sequence from right
-//			int le = s.Length;
-//
-//			for (int i = 0 ; i < le; i++) {
-//				string tr = s.Slice(0, le - i);
-//				if (IsPrime(string tr)) {
-//					Console.WriteLine(tr);
-//				}
-//			}
-//		}
+		private bool IsDeletable(double n) {
+			string s = n.ToString();
+			bool d = true;
+			
+			TruncateLeft(s);
+
+			if (!d) {
+				d = false;
+			}
+			return d;
+		}
 	}
 }
